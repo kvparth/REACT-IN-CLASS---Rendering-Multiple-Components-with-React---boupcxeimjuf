4ebb2c9416fc09eaf96d405cab00ed00374e148c
DESKTOP-JAE89I5\Dell <parthkvk10@gmail.com>
2022-02-20 02:32:08 -0800

    done

---
+++ src/components/description.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export class Description extends React.Component {
+  
+  render() {
+    return (
+      <div data-ns-test attribute="project-description">dhfhf
+        
+      </div>
+    )
+  }
+}
---
+++ src/components/name.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export class Name extends Component {
+  render() {
+    return (
+      <div data-ns-test attribute="project-name">
+          dshfkdhjgkg       
+      </div>
+    )
+  }
+}
+
